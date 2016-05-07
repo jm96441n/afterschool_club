66ed960693810794a0a1287c2cae87611fc2479a
Alyssa <aransbury@gmail.com>
2016-05-07 15:21:33 -0400

    Add font awesome gem, basic template for welcome index view

--- db/migrate/20160507182828_create_programs.rb
+++ db/migrate/20160507182828_create_programs.rb
@@ -1,8 +1,7 @@
 class CreatePrograms < ActiveRecord::Migration
   def change
     create_table :programs do |t|
-      t.bool :actual, null: false #whether program is an idea or an actual program
-      t.string :address
+
       t.timestamps null: false
     end
   end
