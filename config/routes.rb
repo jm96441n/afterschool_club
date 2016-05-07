a33ffee4a2a3bfea40e3b49521fe479c03ce0caa
datmar <darius.atmar@gmail.com>
2016-05-07 18:13:19 -0400

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-
+  get '/login', to: 'sessions#new'
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :programs, only: [:index, :show, :create, :new]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
