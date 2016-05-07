305e8f98c7454d481683dac5a4e5b974790f69ed
datmar <darius.atmar@gmail.com>
2016-05-07 18:13:03 -0400

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_current_user, only: [:new, :create, :oauth_create]
+  skip_before_action :require_current_user, only: [:new, :create]
 
   def new
     @user = User.new
@@ -7,16 +7,18 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-    if @user
+    if @user and @user.authenticate params[:password]
       session[:user_id] = @user.id
+      binding.pry
+      redirect_to '/'
     else
       flash[:alert] = "Incorrect email or password"
-      redirect_to login_path
+      redirect_to '/login'
     end
   end
 
   def destroy
     session.clear
-    redirect_to root_path
+    redirect_to 'root_path'
   end
 end
