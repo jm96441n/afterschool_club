344771047b5a8b36735793bfa67d2ea4ca2fe6c7
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-05-07 19:59:40 -0400

    add approved attr to program model

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,7 +1,17 @@
 class Program < ActiveRecord::Base
+	before_create :set_initial_approval
+
 	has_many :programtags
 	has_many :tags, through: :programtags
 
 	validates :existing, presence: true
 	validates :address, presence: true
+
+	def set_initial_approval
+		if self.existing == true
+			self.approved == true
+		else
+			self.approved == false
+		end
+	end
 end
--- db/migrate/20160507182828_create_programs.rb
+++ db/migrate/20160507182828_create_programs.rb
@@ -2,6 +2,7 @@ class CreatePrograms < ActiveRecord::Migration
   def change
     create_table :programs do |t|
       t.boolean :existing, null: false #whether program is an idea or an actual program
+      t.boolean :approved, null: false
       t.string :address, null: false
       t.timestamps null: false
       t.string :agency
