9a7ff2c18c8b67ea1c7d9575824b2f0fd126ea9a
John Maguire <jmaguire5588@gmail.com>
2016-05-08 03:40:02 -0400

    fix current_user bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,17 @@ class WelcomeController < ApplicationController
   def index
     @programs = Program.where(existing: true).limit(20)
     # @programs = Program.where(existing: true).page(1).per(3)
-    if current_user.role == 'student'
-      @user_programs_attending = [] #update with new table data about programs user is actually attending
-      @favorites = current_user.favorite_programs
-      @user_programs = Program.where(existing: false, approval: 'approved').limit(20)
-      @classmate_programs = Program.where(existing: true).limit(20)
-    elsif current_user.role == 'teacher'
+    if current_user
+      if current_user.role == 'student'
+        @user_programs_attending = [] #update with new table data about programs user is actually attending
+        @favorites = current_user.favorite_programs
+        @user_programs = Program.where(existing: false, approval: 'approved').limit(20)
+        @classmate_programs = Program.where(existing: true).limit(20)
+      elsif current_user.role == 'teacher'
 
-    elsif current_user.role == 'parent'
+      elsif current_user.role == 'parent'
 
+      end
     end
 
   end
