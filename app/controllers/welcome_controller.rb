49facc37ebf5462c48d8934e5fe7896f190b1ecb
Alyssa <aransbury@gmail.com>
2016-05-08 05:21:03 -0400

    Add seeds for attender, favorites, user submissions. Revise welcome controller to account for changes in seed file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,17 +5,17 @@ class WelcomeController < ApplicationController
     # @programs = Program.where(existing: true).page(1).per(3)
     if current_user
       if current_user.role == 'student'
-        @user_programs_attending = [] #update with new table data about programs user is actually attending
+        @user_programs_attending = Attender.where(user_id: current_user.id).map{ |attender| Program.find(attender.program_id) }
         @favorites = current_user.favorite_programs
         @user_programs = Program.where(existing: false, approval: 'approved').limit(20)
         @classmate_programs = Program.where(existing: true).limit(20)
       elsif current_user.role == 'teacher'
-        @favorites = Favorite.all.map{|favorite| Program.find(program_id: favorite.program_id)}
-        # @favorites = Program.where(existing:true).limit(20) 
-        @submitted_programs = Program.where(approval: 'pending')
+        @favorites = Favorite.all.map{ |favorite| Program.find(favorite.program_id) }
+        submissions = UserProgram.all.map{ |program| program.program_id }.compact.uniq
+        @submitted_programs = submissions.map{ |id| Program.find(id) }
       elsif current_user.role == 'parent'
-        @favorites = Program.where(existing:true).limit(20) # parent should only see programs their children have favorited
-        @submitted_programs = Program.where(approval: 'pending')
+        @favorites = Favorite.all.map{ |favorite| Program.find(favorite.program_id) }
+        @submitted_programs = UserProgram.all.map{ |program| Program.find(program.program_id) }
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -157,6 +157,10 @@ end
 
 users = User.all
 
+suggested_program = Program.create(description: "I really want an after school program where I can practice computer science. I don't think we do enough of it in class and I can't find any programs in my area")
+suggestion = UserProgram.create(user_id: User.where(role: 'student').first, program_id: suggested_program.id)
+
+
 popular_programs.each do |program|
 	(3 + rand(14)).times do
 		user = users.sample
