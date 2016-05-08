ee396ca88f50f896ccffcbdef6bda24fa721ece0
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-05-07 23:30:44 -0400

    start working on teacher portal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect :root
+      redirect_to :root
     else
       flash[:alert] = "Please try again"
     end
