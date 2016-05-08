0b273984037a2d274629b5f9f2120ca689aa13fc
John Maguire <jmaguire5588@gmail.com>
2016-05-07 23:00:49 -0400

    submit form and page for submitted entries

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,9 +1,9 @@
 class Program < ActiveRecord::Base
 	before_create :set_initial_approval
-	
 
-	validates :existing, presence: true
-	validates :address, presence: true
+
+	# validates :existing, presence: true
+
 
 	def set_initial_approval
 		if self.existing == true
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/login', to: 'sessions#new'
+  get '/programs/submitted_programs', to: 'programs#submitted_programs'
   resources :users, only: [:new, :create, :show, :edit, :update]
-  resources :programs, only: [:index, :show, :create, :new]
+  resources :programs
   resources :sessions, only: [:new, :create, :destroy]
 
 end
