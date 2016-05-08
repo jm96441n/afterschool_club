e8fc3ee134db9bd3a190a76cbb158ab25eb1b44c
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-05-07 20:35:06 -0400

    commit before pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507192009) do
+ActiveRecord::Schema.define(version: 20160507235236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160507192009) do
 
   create_table "programs", force: :cascade do |t|
     t.boolean  "existing",   null: false
+    t.boolean  "approved",   null: false
     t.string   "address",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -55,6 +56,11 @@ ActiveRecord::Schema.define(version: 20160507192009) do
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
+
+  create_table "user_programs", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "program_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
