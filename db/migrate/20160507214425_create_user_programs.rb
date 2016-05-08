ae30b7c9fa3fcd47a460e475a6c031c390914531
John Maguire <jmaguire5588@gmail.com>
2016-05-07 21:27:15 -0400

    fixed migrations

--- db/migrate/20160507214425_create_user_programs.rb
+++ db/migrate/20160507214425_create_user_programs.rb
@@ -1,10 +1,10 @@
 class CreateUserPrograms < ActiveRecord::Migration
   def change
     create_table :user_programs do |t|
-    	t.references :users
-    	t.references :programs
+    	t.references :user
+    	t.references :program
 
-    	t.timestamps, null: false
+    	t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507235236) do
+ActiveRecord::Schema.define(version: 20160507214425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20160507235236) do
     t.float    "long"
   end
 
+  create_table "user_programs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "program_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
