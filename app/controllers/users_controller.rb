be9d28142a928827c0c4d3ef48a2c9a0146dcd86
Alyssa <aransbury@gmail.com>
2016-05-07 22:44:20 -0400

    User login and register forms working from homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      redirect :root
     else
-      render :new
+      flash[:alert] = "Please try again"
     end
   end
 
